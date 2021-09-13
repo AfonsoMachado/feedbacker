6e75f1244854caede704b5331a6e34f196821751
Afonso Machado <afonso777@hotmail.com>
2021-09-13 09:11:37 -0300

    preparando estruturas de pastas - finalizando configuração do css

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const Home = () => import('')
-const Feedbacks = () => import('')
-const Credentials = () => import('')
+// importando componentes a serem usados
+const Home = () => import('../views/Home/index.vue')
+const Feedbacks = () => import('../views/Feedbacks/index.vue')
+const Credentials = () => import('../views/Credentials/index.vue')
 
 const routes = [
   {
