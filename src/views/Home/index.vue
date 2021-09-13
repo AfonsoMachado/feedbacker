39c60359f15c5a76e70c389e50bff7fe7a33a27b
Afonso Machado <afonso777@hotmail.com>
2021-09-13 11:21:17 -0300

    teste de commit

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1 class="font-black text-brand-main">Aqui é a Home</h1>
+  <custom-header></custom-header>
+  <contact></contact>
+
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">
+      feedbacker © 2021 teste de commit
+    </p>
+  </div>
 </template>
 
 <script>
