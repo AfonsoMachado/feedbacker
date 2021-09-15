f57c02bcccddec0a4bf9bff25b48d658c2c76057
Afonso Machado <afonso.machado@blockhub.builders>
2021-09-15 17:03:24 -0300

    pagina de contato

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,14 +1,47 @@
 <template>
-  <custom-header></custom-header>
-  <contact></contact>
+  <!-- eventos emitidos para as funções  -->
+  <custom-header @create-account="handleAccountCreate" @login="handleLogin" />
+  <contact />
 
   <div class="flex justify-center py-10 bg-brand-gray">
-    <p class="font-medium text-center text-gray-800">
-      feedbacker © 2021 teste de commit
-    </p>
+    <p class="font-medium text-center text-gray-800">feedbacker © 2021</p>
   </div>
 </template>
 
 <script>
-export default {}
+import CustomHeader from '../Home/CustomHeader.vue'
+import Contact from '../Home/Contact.vue'
+import { useRouter } from 'vue-router'
+import { onMounted } from 'vue'
+
+export default {
+  // componentes usados no negocio
+  components: {
+    CustomHeader,
+    Contact,
+  },
+  setup() {
+    // para usar o router, semelhante ao react
+    const router = useRouter()
+
+    // quando o componente é montado
+    onMounted(() => {
+      // capturando token do local storate
+      const token = window.localStorage.getItem('token')
+      // se tiver um token, manda para a tela de feedbacks
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handleLogin() {}
+
+    function handleAccountCreate() {}
+
+    return {
+      handleLogin,
+      handleAccountCreate,
+    }
+  },
+}
 </script>
