c45fd4ee7edbf3300afa79046756e5daf54a5f89
Afonso Machado <afonso.machado@blockhub.builders>
2022-05-24 11:00:24 -0300

    create modal factory

---
+++ src/components/ModalFactory/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <!-- Local de renderização do componente -->
+  <teleport to="body">
+    <!-- Overlay -->
+    <div
+      v-if="state.isActive"
+      class="
+        fixed
+        top-0
+        left-0
+        z-50
+        flex
+        items-center
+        justify-center
+        w-full
+        h-full
+        bg-black bg-opacity-50
+      "
+      @click="handleModalToogle({ status: false })"
+    >
+
+      <div
+        class="fixed mx-10"
+        :class="state.width"
+        @click.stop
+      >
+        <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
+          <div class="flex flex-col px-12 py-10 bg-white">
+            <!-- Componente dinamico para renderizar  -->
+            <component :is="state.component" />
+          </div>
+        </div>
+
+    </div>
+  </teleport>
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+
+const DEFAULT_WIDTH = 'w-3/4 lg:w-1/3'
+export default {
+  setup() {
+    // Declarando componente reativo
+    const state = reactive({
+      isActive: false,
+      component: {},
+      props: {},
+      width: DEFAULT_WIDTH,
+    })
+
+    function handleModalToogle({ status }) {}
+
+    return {
+      state,
+      handleModalToogle,
+    }
+  },
+}
+</script>
+/
