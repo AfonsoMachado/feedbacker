3d4389ea6b1ca70989ee497bf1f90e3615b31a09
Afonso Machado <afonsosmachado@gmail.com>
2022-08-04 14:10:59 -0300

    get logged user

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,35 @@
 </template>
 
 <script>
+import { watch } from 'vue'
+import { useRoute } from 'vue-router'
 import ModalFactory from './components/ModalFactory'
+import router from './router'
+import services from './services'
 
 export default {
   components: {
     ModalFactory,
+  },
+  setup() {
+    const route = useRoute()
+
+    watch(
+      () => route.path,
+      async () => {
+        if (route.meta.hasAuth) {
+          const token = window.localStorage.getItem('token')
+
+          if (!token) {
+            router.push({ name: 'Home' })
+            return
+          }
+
+          const { data } = await services.users.getMe()
+          console.log(data)
+        }
+      }
+    )
   },
 }
 </script>
